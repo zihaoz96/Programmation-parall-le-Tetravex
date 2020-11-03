75db663806bfb526b5d1cd14e1ca5cc1447a490f
ZihaoZHENG <zihaoz96@gmail.com>
2020-11-03 12:28:00 +0100

    Update

--- mini-projet/Chess.h
+++ mini-projet/Chess.h
@@ -4,7 +4,6 @@ using namespace std;
 
 class Chess{
 public:
-	int id = -1;
 	int top;
 	int bot;
 	int right;
@@ -13,19 +12,10 @@ public:
 
 
 	
-	void setValue(int id, int left, int top, int right, int bot){
-		this->id = id;
+	void setValue(int left, int top, int right, int bot){
 		this->top = top;
 		this->bot = bot;
 		this->left = left;
 		this->right = right;
-	}
-
-	bool empty(){
-		if (this->id == -1)
-		{
-			return true;
-		}
-		return false;
 	}
 };
--- mini-projet/Game.cpp
+++ mini-projet/Game.cpp
@@ -6,7 +6,6 @@
 #include <condition_variable>
 #include <mutex>
 #include <queue>
-#include <map>
 
 using namespace std;
 
@@ -15,20 +14,28 @@ class Game
 public:
 	int N;
 	vector<Chess> allChess;
-	Chess table[100];
+	Chess T[100];
 	
 	vector<thread> threads;
+	queue<int> queueJob;
 	bool terminate = false;
-	queue<int> queueIndex;
 	mutex m;
 	condition_variable cond_var;
 	
 	Game(int N, vector<Chess> allChess){
 		this->N = N;
 		this->allChess = allChess;	
+		for (int i = 0; i < N; ++i)
+		{
+			for (int j = 0; j < N; ++j)
+			{
+				queueJob.push(N*i+j);
+			}
+		}
 	}
 	
 	~Game(){
+
 		terminate = true;
 
 		cond_var.notify_all();
@@ -39,91 +46,73 @@ public:
 				thread.join();
 			}
 		}
-  
-  		affiche();
 	}
-	
 
-	void affichage(){
+	void affichage(Chess table[100]){
 		for (int i = 0; i < N; ++i)
 		{
 			for (int j = 0; j < N; ++j)
 			{
-				cout<<table[i*N+j].id<<",";
+				cout<<"———————";
+			}
+			cout<<endl;
+			for (int k = 0; k < N; ++k)
+			{
+				cout<<"|  "<<table[i*N+k].top<<"  |";
+			}
+			cout<<endl;
+			for (int h = 0; h < N; ++h)
+			{
+				cout<<"|"<<table[i*N+h].left<<"   "<<table[i*N+h].right<<"|";
+			}
+			cout<<endl;
+			for (int l = 0; l < N; ++l)
+			{
+				cout<<"|  "<<table[i*N+l].bot<<"  |";
+			}
+			cout<<endl;
+			for (int o = 0; o < N; ++o)
+			{
+				cout<<"———————";
 			}
 			cout<<endl;
 		}
 	}
 
-	// Version de backtracking séquentielle
-	bool backtrackingSequentielle(int position){
+	// Backtracking séquentielle
+	bool backtrackingSequentielle(Chess table[100], int position){
 		if (position==N*N)
 		{
+			affichage(table);
 			return true;
 		}
-
+		// Cycle all the pieces
 		for (int i = 0; i < N*N; ++i){
-
+			// No used 
 			if (!allChess[i].used){
-
-				if (checkPosition(allChess[i], position)){
+				// Right position
+				if (checkPosition(table, allChess[i], position)){
+					// Set this chess used
 					allChess[i].used = true;
-					
+					// Set chess in this position
 					table[position] = allChess[i];
-					
-					if (backtrackingSequentielle(position+1)){
+					// Recusif
+					if (backtrackingSequentielle(table, position+1)){
 						return true;
 					}
-
+					// Set this chess NO used
 					allChess[i].used = false;
 				}	
-				
 			}
 		}
 		return false;
 	}
 
-	
-	// Version de backtracking parallèles
-	void startTheGame(){
-		for (int i = 0; i < N*N ; ++i)
-		{
-			threads.push_back(std::thread(&Game::doWork,this));
-		}
-	}
-
-	void doWork(){
-		
-		/*while(!terminate){
-			int indexTable;
-			{
-				unique_lock<mutex> lok(m);
-
-				cond_var.wait(lok, [&]{return !queueIndex.empty() || terminate;});
-
-				if (terminate)
-				{
-					break;
-				}
-
-				indexTable = queueIndex.front();
-
-				queueIndex.pop();
-			}
-			checkJob(indexTable);
-		}*/
-	}
-
-	// job is id of Chess
-	void checkJob(int indexTable){
-		
-
-	}
-	
 	/*
-	 * Check is good position
+	 * Verifier c'est bon position
+	 * Pour verifier en version de backtracking séquentielle
 	 */
-	bool checkPosition(Chess chess, int position){
+	bool checkPosition(Chess table[100], Chess chess, int position){
 
 		// if top is not out of table
 		if (position-N >= 0)
@@ -146,6 +135,45 @@ public:
 
 		return true;
 	}
+
+
+	
+	// Version de backtracking parallèles
+	void startTheGame(){
+		for (int i = 0; i < N ; ++i)
+		{
+			threads.push_back(std::thread(&Game::doWork,this));
+		}
+	}
+
+	void doWork(){
+		Chess table[100];
+
+		while(!terminate){
+			int job;
+			{
+				unique_lock<mutex> lok(m);
+
+				cond_var.wait(lok, [&]{return !queueJob.empty() || terminate;});
+
+				if (terminate)
+				{
+					break;
+				}
+
+				job = queueJob.front();
+
+				queueJob.pop();
+			}
+
+			if(backtrackingSequentielle(table, job)){
+				
+				terminate = true;
+			}
+
+			cond_var.notify_one();
+		}
+	}
 };
 
 
@@ -167,7 +195,7 @@ int main(int argc,char** argv)
 			file >>	right;
 			file >> bot;
 			
-			c.setValue(i,left,top,right,bot);
+			c.setValue(left,top,right,bot);
 
 			allChess.push_back(c);
 		}
@@ -176,8 +204,21 @@ int main(int argc,char** argv)
 	}
 
 	Game game(N,allChess);
-	game.backtrackingSequentielle(0);
-	game.affichage();
-	
+
+	int op;
+	cout<<"Entre 1 pour test backtracking Sequentielle"<<endl<<"Entre 2 pour test backtracking parallele"<<endl;
+	cin>>op;
+
+	//backtracking Sequentielle
+	if (op == 1)
+	{
+		game.backtrackingSequentielle(game.T,0); 
+	} 
+	//backtracking parallele
+	else
+	{
+		game.startTheGame();
+	}
+
 	return 0;
 }
